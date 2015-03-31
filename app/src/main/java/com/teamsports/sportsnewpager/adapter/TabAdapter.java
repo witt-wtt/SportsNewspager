d2d51ae613bf8bdce8cf6bdbef5d1b05f5fb0c4d
Demon <515386338@qq.com>
2015-03-31 09:15:11 +0800

    hotnews data loading

--- app/src/main/java/com/teamsports/sportsnewpager/adapter/TabAdapter.java
+++ app/src/main/java/com/teamsports/sportsnewpager/adapter/TabAdapter.java
@@ -12,29 +12,20 @@ import com.teamsports.sportsnewpager.fragment.MainFragment;
  */
 public class TabAdapter extends FragmentPagerAdapter {
     public static final String[] TITLES = new String[]{"热门", "NBA", "国际足球", "中国足球", "CBA", "综合体育"};
-    public static final int [] feed_id = new int[]{1009,653,654,655,656,657};
+    public static final int[] feed_id = new int[]{1009, 653, 654, 655, 656, 657};
+
     public TabAdapter(FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int position) {
-       if (position==0){
-           MainFragment fragment = new MainFragment();
-           Bundle bundle = new Bundle();
-           bundle.putInt("feed_id",feed_id[position+5]);
-           bundle.putString("TITLES",TITLES[position]);
-           fragment.setArguments(bundle);
-           return fragment;
-       }else{
-           MainFragment fragment = new MainFragment();
-           Bundle bundle = new Bundle();
-           bundle.putInt("feed_id",feed_id[position]);
-           bundle.putString("TITLES",TITLES[position]);
-           fragment.setArguments(bundle);
-           return fragment;
-       }
-
+        MainFragment fragment = new MainFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("feed_id", feed_id[position]);
+        bundle.putString("TITLES", TITLES[position]);
+        fragment.setArguments(bundle);
+        return fragment;
     }
 
     @Override
